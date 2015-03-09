40663895d33f53100743c27574b0229f93571b5d
Michael Mattson <michael@mmattson.com>
2015-03-08 23:20:01 -0400

    Yay partition

--- 05-Selection/selection.java
+++ 05-Selection/selection.java
@@ -1,37 +1,52 @@
 import java.util.*;
-import java.io.*;
 
 public class selection {
     
-    public String partition (int[] L, int SI, int EI) {
-        int[] D = new int[L.length];
-        int p = L[SI];
-        for (int x = 0; x < L.length;x++){
-            if (x < SI && x > EI){
-                D[x] = L[x];
-            }
+    private int[] L;
+    private Random rn = new Random();
+    
+    public selection(int[] n) {
+        L = new int[n.length];
+        for (int i = 0; i < n.length; i++) {
+            L[i] = n[i];
         }
-        SI++;
-        while (SI<EI){
-            if (L[SI]<p){
-                D[SI]=L[SI];
+    }
+    
+    public int partition(int[] L, int k, int SI, int EI) {
+        int P = L[EI];
+        int low = SI;
+        int high = EI;
+        int highComp, lowComp;
+        while (low < high) {
+            if (L[low] < P) {
+                low = low + 1;
+            } else {
+                high = high - 1;//You found an item thats bigger than the p, so put it on the other side.
+                lowComp = L[low];
+                highComp = L[high];
+                L[low] = highComp;
+                L[high] = lowComp;
                 
             }
-            else{
-                D[EI]=L[SI];
-                EI--;
-                
-            }
-            SI++;
         }
-        D[SI]=p;
-        return D.toString();
+        L[EI] = L[high];
+        L[high] = P;
+        if (high == k) {
+            return L[high];//You found it!
+        } else if (high < k) {
+            return partition(L,k,SI+1,EI);
+        } else {
+            return partition(L,k,SI,EI - 1);//Change the search
+        }
     }
-
-
-    public static void main(String[] args){
-        selection x = new selection();
-        int[] L = {15,4,6,8,3,7,9,24,28,2};
-        System.out.println(x.partition(L, 0, 9));
+    
+    public static void main(String[] args) {
+        int[] L = new int[20];
+        Random rn = new Random();
+        for (int i = 0; i < L.length; i++) {
+            L[i] = rn.nextInt(20);
+        }
+        selection s = new selection(L);
+        System.out.println(s.partition(L,7,0,19));
     }
-}//I think that this should all work out, but when I run it I get [I@7a14b43d, so clearly something's wrong.
+}
