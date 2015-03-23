463563f0a817034aacf2bbb9bb24dd9daa68c2cd
Michael Mattson <michael@mmattson.com>
2015-03-22 22:31:56 -0400

    I don't understand the error, but I think the function would work

---
+++ 09-Iterators/LLit.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class LLit<E> implements Iterator<E>{
+    private Node<E> t;
+    public LLit(Node<E> n){
+        t=n;
+    }
+    
+    public boolean hasNext(){
+        return t!=null;
+    }
+    
+    public E next(){
+        E retval = t.getData();
+        t=t.getNext();
+        return retval;
+    }
+    
+    public void remove() {
+        if (t.getNext() == null)
+            t = null;
+        }else{
+            t.setNext(t.getNext());
+    }
+}
