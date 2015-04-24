b15ea80cdd794f525b47ca82ba6576520c2d63a0
Michael Mattson <michael@mmattson.com>
2015-04-23 22:12:44 -0400

    please work

--- 12a-ClassMaze/Maze.java
+++ 12a-ClassMaze/Maze.java
@@ -1,20 +1,20 @@
 import java.io.*;
 import java.util.*;
 
-public class Maze
-{
+public class Maze {
     private char[][] board;
     private int maxX;
     private int maxY;
-    
-    private char path='#';
-    private char wall=' ';
-    private char me='z';
-    private char exit='$';
+    private char wall = ' ';
+    private char exit = '$';
+    private char exitX;
+    private char exitY;
+    private char path = '#';
+    private char me = 'C';
     private char visited = '.';
     private boolean solved = false;
     
-    private Frontier f;
+    private LinkedList<Node> l;
     
     public void delay(int n){
         try {
@@ -22,22 +22,22 @@ public class Maze
         } catch (Exception e) {}
     }
     
-    public Maze()
-    {
+    public Maze() {
         maxX=40;
         maxY=20;
         board = new char[maxX][maxY];
         
         try {
-            
             Scanner sc = new Scanner(new File("maze.dat"));
             int j=0;
-            while (sc.hasNext())
-            {
+            while (sc.hasNext()) {
                 String line = sc.nextLine();
-                for (int i=0;i<maxX;i++)
-                {
+                for (int i=0;i<maxX;i++) {
                     board[i][j] = line.charAt(i);
+                    if (board[i][j] == exit) {
+                        exitX = i;
+                        exitY = j;
+                    }
                 }
                 j++;
             }
@@ -48,91 +48,51 @@ public class Maze
         
     }
     
-    public String toString()
-    {
+    public String toString() {
         String s = "[2J\n";
-        
         for (int y=0;y<maxY;y++)
         {
             for (int x=0;x<maxX;x++)
                 s = s +board[x][y];
             s=s+"\n";
         }
+        
         return s;
     }
     
-    /*
-     solved - instance variable to indicate we're done
-     
-     */
-    public void solve(int x, int y){
-        if (board[x][y]==wall ||
-            board[x][y]==me ||
-            board[x][y]==visited ||
-            solved){
-            return;
-        }
-        
-        if (board[x][y]==exit){
-            System.out.println(this);
-            solved = true;
-        }
-        delay(100);
-        System.out.println(this);
-        board[x][y]=me;
-        solve(x+1,y);
-        solve(x-1,y);
-        solve(x,y+1);
-        solve(x,y-1);
-        if (!solved){
-            board[x][y]=visited;
-        }
-    }
-    
-    /*
-     Only adds if the tx,ty spot is available path or exit
-     */
-    public void addToFront(int tx,int ty, Node current){
+    public void addToFront(int tmpX, int tmpY, Nde current) {
         Node tmp = null;
-        if (board[tx][ty]=='#' || board[tx][ty]=='$'){
-            tmp = new Node(tx,ty);
+        if (board[tmpX][tmpY] == path || board[tmpX][tmpY] == exit) {
+            tmp = new Node(tmpX,tmpY,current.getSpaces()+1);
+            tmp.AStarSearch(26,18);
             tmp.setPrev(current);
             f.add(tmp);
         }
-        
     }
     
-    public void bfs(int x, int y){
-        //f = new Frontier();
-        f = new StackFront();
-        
+    public void bestSearch(int x, int y) {
+        f = new Frontier();
         f.add(new Node(x,y));
-        
-        int tx=0,ty=0;
+        int tmpX = 0;
+        int tmpY = 0;
         Node current = null;
-        while (!f.isEmpty()){
+        while(!f.isEmpty()) {
             current = f.remove();
-            int cx = current.getX();
-            int cy = current.getY();
-            
-            if (board[cx][cy]=='$')
+            int currentX = current.getX();
+            int currentY = current.getY();
+            if (board[currentX][currentY] == exit) {
                 break;
-            
-            board[cx][cy]='z';
-            
-            addToFront(cx+1,cy,current);
-            addToFront(cx-1,cy,current);
-            addToFront(cx,cy+1,current);
-            addToFront(cx,cy-1,current);
-     
-            delay(50);
+            }
+            board[currentX][currentY] = me;
+            addToFront(currentX+1,currentY,current);
+            addToFront(currentX-1,currentY,current);
+            addToFront(currentX,currentY+1,current);
+            addToFront(currentX,currentY-1,current);
             System.out.println(this);
         }
         
-        // path recovery
-        for (Node p = current.getPrev(); p != null ; p = p.getPrev()){
+        for(Node p = current.getPrev(); p != null; p = p.getPrev()) {
             board[p.getX()][p.getY()] = 'P';
-            delay(100);
             System.out.println(this);
         }
     }
@@ -140,8 +100,7 @@ public class Maze
     public static void main(String[] args){
         Maze m = new Maze();
         System.out.println(m);
-        m.bfs(1,1);
-        System.out.println(m);
-        
+        m.bestSearch(1,1);
     }
+    
 }
